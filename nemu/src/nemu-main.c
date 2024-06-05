443518c1cf287897b28208d6188d97a8dabd51cb
tracer-ics2023 <tracer@njuics.org>
2024-06-05 16:40:01 +0800

    >  compile NEMU 221220000 张三 Linux rache-Mint 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:40:01 up  1:07,  1 user,  load average: 0.88, 1.09, 1.04

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -14,23 +14,48 @@
 ***************************************************************************************/
 
 #include <common.h>
-#include "expr_tester.h"
+#include "./monitor/sdb/sdb.h"
 
 void init_monitor(int, char *[]);
 void am_init_monitor();
 void engine_start();
 int is_exit_status_bad();
 
+void init_sdb();
+
+void test_expr() {
+    FILE *fp = fopen("./src/test_expr/examples.txt", "r");
+    assert(fp != NULL);
+    init_sdb();
+    
+    unsigned int answer;
+    char expression[256];
+    bool success;
+    while (fscanf(fp, "%255s %u", expression, &answer))
+    {
+        printf("%s %u\n", expression, answer);
+        unsigned int result = expr(expression, &success);
+        if (!success) {
+            printf("success=false: expr=%s\n", expression);
+        }
+        if (result != answer) {
+            printf("Result is incorrect: expr=%s, res=%u, ans=%u\n", expression, result, answer);
+        }
+    }
+    fclose(fp);
+    printf("PASSED\n");
+}
+
 int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
-// #ifdef CONFIG_TARGET_AM
-//   am_init_monitor();
-// #else
-//   init_monitor(argc, argv);
-// #endif
+#ifdef CONFIG_TARGET_AM
+  am_init_monitor();
+#else
+  init_monitor(argc, argv);
+#endif
 
-//   /* Start engine. */
-//   engine_start();
+  /* Start engine. */
+  //engine_start();
   test_expr();
 
   return is_exit_status_bad();
