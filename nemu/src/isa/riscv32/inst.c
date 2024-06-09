9d4c163258f512028db5f31c18451d71927bcd0a
tracer-ics2023 <tracer@njuics.org>
2024-06-09 18:04:49 +0800

    >  compile NEMU 221220000 张三 Linux racheMint 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:04:49 up 19:32,  1 user,  load average: 0.87, 0.63, 0.63

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -33,7 +33,7 @@ enum {
 #define immU() do { *imm = SEXT(BITS(i, 31, 12), 20) << 12; } while(0)
 #define immS() do { *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); } while(0)
 #define immB() do { *imm = (SEXT(BITS(i, 31, 31), 1) << 12) | (SEXT(BITS(i, 30, 25), 6) << 5) | (SEXT(BITS(i, 11, 8), 4) << 1) | (SEXT(BITS(i, 7, 7), 1) << 11); } while (0)
-#define immJ() do { *imm = (SEXT(BITS(i, 31, 31), 1) << 20) | (SEXT(BITS(i, 30, 21), 10) << 1) | (SEXT(BITS(i, 20, 20), 1) << 11) | (SEXT(BITS(i, 19, 12), 8) << 12);} while(0)
+#define immJ() do { *imm = (SEXT(BITS(i, 31, 31), 1) << 20) | (SEXT(BITS(i, 30, 21), 10) << 1) | (SEXT(BITS(i, 20, 20), 1) << 11) | (SEXT(BITS(i, 19, 12), 8) << 12); } while(0)
 
 static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_t *imm, int type) {
   uint32_t i = s->isa.inst.val;
@@ -46,6 +46,10 @@ static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_
     case TYPE_S: src1R(); src2R(); immS(); break;
     case TYPE_B: src1R(); src2R(); immB(); break;
     case TYPE_J:                   immJ(); break;
+  }
+  if (type == TYPE_J) {
+    Log("%x\n", (uint32_t)(SEXT(BITS(i, 31, 31), 1) << 20));
+    Log("%x\n", (uint32_t)(SEXT(BITS(i, 30, 21), 10) << 1));
   }
 }
 
