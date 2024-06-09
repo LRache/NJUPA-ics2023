ec75321cd8a8a000b79bfeddfe0f3a17b9cfd468
tracer-ics2023 <tracer@njuics.org>
2024-06-09 18:47:34 +0800

    >  compile NEMU 221220000 张三 Linux racheMint 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:47:34 up 20:15,  1 user,  load average: 0.67, 0.76, 0.76

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -52,6 +52,7 @@ static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_
     Log("imm[10:5]=%x", (uint32_t)(BITS(i, 30, 25)) << 5);
     Log("imm[4:1]=%x", (uint32_t)(BITS(i, 11, 8)) << 1);
     Log("imm[11]=%x", (uint32_t)(BITS(i, 7, 7) << 11));
+    Log("0x%x", *imm);
   }
 }
 
