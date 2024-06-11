3c66d84450c8c23e796200c79711a2a2a58cdd97
tracer-ics2023 <tracer@njuics.org>
2024-06-11 22:36:56 +0800

    >  compile NEMU 221220000 张三 Linux rache-Mint 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:36:56 up 18 min,  1 user,  load average: 0.28, 0.32, 0.24

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -121,7 +121,7 @@ static int parse_args(int argc, char *argv[]) {
     {"type"     , required_argument, NULL, 't'},
     {0          , 0                , NULL,  0 },
   };
-  char imgTypeStr[12] = "img";
+  char *imgTypeStr;
   int o;
   while ( (o = getopt_long(argc, argv, "-bhl:d:p:t", table, NULL)) != -1) {
     switch (o) {
@@ -129,7 +129,7 @@ static int parse_args(int argc, char *argv[]) {
       case 'p': sscanf(optarg, "%d", &difftest_port); break;
       case 'l': log_file = optarg; break;
       case 'd': diff_so_file = optarg; break;
-      case 't': sscanf(optarg, "%s", imgTypeStr); break;
+      case 't': imgTypeStr = optarg; break;
       case 1: img_file = optarg; return 0;
       default:
         printf("Usage: %s [OPTION...] IMAGE [args]\n\n", argv[0]);
