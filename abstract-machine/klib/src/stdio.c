297ee4f91a32c9f88dfc2303d1805c34f270f6f0
tracer-ics2023 <tracer@njuics.org>
2024-06-11 18:46:01 +0800

    >  compile NEMU 221220000 张三 Linux rache-Mint 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:46:01 up 19 min,  1 user,  load average: 0.58, 0.40, 0.28

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -10,11 +10,44 @@ int printf(const char *fmt, ...) {
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
+  char *p = (char*) fmt;
+  char *ori = out;
+  while (*p)
+  {
+    if (*p == '%') {
+      p++;
+      if (*p == '%') {
+        *(out++) = *(p++);
+      } else if (*p == 's') {
+        char *s = va_arg(ap, char*);
+        while (*s) *(out++) = *(s++);
+        p++;
+      } else if (*p == 'd') {
+        int d = va_arg(ap, int);
+        char q[13] = {};
+        char *t = q;
+        while (d) {
+          *(t++) = d % 10 + '0';
+          d = d / 10;
+        }
+        char *h = q;
+        while (h<=t) *(out++) = *(h++);
+      }
+    } else {
+      *(out++) = *(p++);
+    }
+  }
+  *out = 0;
+  return out - ori - 1;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
   panic("Not implemented");
+  va_list ap;
+  va_start(ap, fmt);
+  int result = vsprintf(out, fmt, ap);
+  va_end(ap);
+  return result;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
