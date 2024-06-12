25f6f603e7a53fc148aefe026222af5773d36b06
tracer-ics2023 <tracer@njuics.org>
2024-06-12 20:11:04 +0800

    >  compile NEMU 221220000 张三 Linux rache-Mint 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:11:04 up 10:22,  1 user,  load average: 0.26, 0.17, 0.10

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -23,8 +23,8 @@ compile_git:
 $(BINARY):: compile_git
 
 # Some convenient rules
-INPUT_FILE=$(NEMU_HOME)/../am-kernels/tests/cpu-tests/build/recursion-riscv32-nemu.elf --t=elf
-#INPUT_FILE=$(NEMU_HOME)/test.bin
+#INPUT_FILE=$(NEMU_HOME)/../am-kernels/tests/cpu-tests/build/recursion-riscv32-nemu.elf --t=elf
+INPUT_FILE=$(NEMU_HOME)/test.bin
 override ARGS ?= --log=$(BUILD_DIR)/nemu-log.txt ${INPUT_FILE}
 override ARGS += $(ARGS_DIFF)
 
