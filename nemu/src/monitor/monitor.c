f6f552557c8ee7c12000baa5d1a8db52b0c1595f
tracer-ics2023 <tracer@njuics.org>
2024-06-12 16:37:17 +0800

    >  compile NEMU 221220000 张三 Linux rache-Mint 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:37:17 up  6:48,  1 user,  load average: 0.32, 0.36, 0.29

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -114,12 +114,12 @@ static long load_elf() {
     if (shdr.sh_flags & SHF_ALLOC) {
       size += shdr.sh_size;
       if (shdr.sh_type == SHT_PROGBITS) {
+        Log("Load section %s", sectionName);
         fseek(fp, shdr.sh_offset, SEEK_SET);
         r = fread(guest_to_host(shdr.sh_addr), size, 1, fp);
         fseek(fp, shdr.sh_offset, SEEK_SET);
         r = fread(bytes+shdr.sh_addr-RESET_VECTOR, size, 1, fp);
         Assert(r == 1, "Read error.");
-        Log("Load section %s", sectionName);
       }
     }
   }
