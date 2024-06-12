d4a270d33b1cdf61aa5be58f2bb99940047c0a8b
tracer-ics2023 <tracer@njuics.org>
2024-06-12 20:15:19 +0800

    >  compile NEMU 221220000 张三 Linux rache-Mint 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:15:19 up 10:26,  1 user,  load average: 0.42, 0.40, 0.21

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -125,7 +125,7 @@ static long load_elf() {
   FILE *fp2 = fopen("/home/rache/Documents/code/ics2023/nemu/test.bin", "w");
   r = fwrite(guest_to_host(RESET_VECTOR), 1, 800, fp2);
   fclose(fp2);
-  Assert(r == 1024, "Write error");
+  Assert(r == 800, "Write error");
   
   fclose(fp);
   free(stringTable);
