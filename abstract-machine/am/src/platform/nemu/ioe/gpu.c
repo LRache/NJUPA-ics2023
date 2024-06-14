6394f9ca27ef17590b976f5ad63a36eeee5b2312
tracer-ics2023 <tracer@njuics.org>
2024-06-14 18:54:00 +0800

    >  compile NEMU 221220000 张三 Linux racheMint 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:54:00 up 56 min,  1 user,  load average: 1.26, 1.02, 0.58

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -38,7 +38,8 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   int w = gpuConfig.width; 
   int h = gpuConfig.height;
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  for (i = 0; i < w * h; i ++) fb[i] = i;
+  uint32_t *pixels = (uint32_t *)ctl->pixels;
+  for (i = 0; i < w * h; i ++) fb[i] = pixels[i];
   outl(SYNC_ADDR, 1);
 }
 
