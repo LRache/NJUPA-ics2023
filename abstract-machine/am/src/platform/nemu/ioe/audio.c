a10c39cf82f77719e405129b6f344b7e992d4fba
tracer-ics2023 <tracer@njuics.org>
2024-06-14 23:50:16 +0800

    >  compile NEMU 221220000 张三 Linux racheMint 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:50:16 up  5:53,  1 user,  load average: 1.52, 1.19, 1.05

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -31,12 +31,13 @@ void __am_audio_status(AM_AUDIO_STATUS_T *stat) {
 
 void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
   uint32_t len = ctl->buf.end - ctl->buf.start;
-  uint32_t count = *(uint32_t*)AUDIO_COUNT_ADDR;
+  volatile uint32_t count = *(uint32_t*)AUDIO_COUNT_ADDR;
+  printf("1\n");
   while (count > len) count = *(uint32_t*)AUDIO_COUNT_ADDR;
+  printf("2\n");
 
   volatile uint8_t *dst = (uint8_t*)AUDIO_SBUF_ADDR;
   volatile uint8_t *src = (uint8_t*)ctl->buf.start;
-  printf("%d\n", (int32_t)*src);
   for (int i = 0; i < len; i++, dst++, src++) {
     *dst = *src;
   }
