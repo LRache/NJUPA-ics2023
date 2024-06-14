9c2b74de1051fb0ba575982f8646c79c38954908
tracer-ics2023 <tracer@njuics.org>
2024-06-14 13:24:08 +0800

    >  compile NEMU 221220000 张三 Linux rache-Mint 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:24:08 up 21:46,  1 user,  load average: 0.34, 0.51, 0.56

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -93,6 +93,7 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
     if (*p == '%') {
       p++;
       for (int i = 0; i < FMT_TABLE_LEN; i++) {
+        putch(*p);
         if (strncmp(p, fmtTable[i].fmt, fmtTable[i].length) == 0) {
           fmtTable[i].fun(&out, ap);
           p += fmtTable[i].length;
