7d96457e02500ca1cb163dede43398e071edc9b3
tracer-ics2023 <tracer@njuics.org>
2024-06-14 16:31:28 +0800

    >  compile NEMU 221220000 张三 Linux rache-Mint 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:31:28 up 1 day, 54 min,  1 user,  load average: 0.82, 0.61, 0.52

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -3,13 +3,25 @@
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
+void __am_gpu_config(AM_GPU_CONFIG_T *cfg);
 void __am_gpu_init() {
+  AM_GPU_CONFIG_T cfg;
+  __am_gpu_config(&cfg);
+  int i;
+  int w = cfg.width; 
+  int h = cfg.height;
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  for (i = 0; i < w * h; i ++) fb[i] = i;
+  outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
+  uint32_t screenSize = inw(VGACTL_ADDR);
+  int width = screenSize >> 16;
+  int height = screenSize & 0x7fff;
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = 0, .height = 0,
+    .width = width, .height = height,
     .vmemsz = 0
   };
 }
