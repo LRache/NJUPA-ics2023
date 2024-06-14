ad7ff87f0ae3ab9a586620b1a9c07ce85745df7f
tracer-ics2023 <tracer@njuics.org>
2024-06-14 23:39:20 +0800

    >  compile NEMU 221220000 张三 Linux racheMint 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:39:20 up  5:42,  1 user,  load average: 1.17, 0.89, 0.91

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -1,5 +1,6 @@
 #include <am.h>
 #include <nemu.h>
+#include "klib.h"
 
 #define AUDIO_FREQ_ADDR      (AUDIO_ADDR + 0x00)
 #define AUDIO_CHANNELS_ADDR  (AUDIO_ADDR + 0x04)
@@ -9,18 +10,31 @@
 #define AUDIO_COUNT_ADDR     (AUDIO_ADDR + 0x14)
 
 void __am_audio_init() {
+  *(uint32_t*)AUDIO_INIT_ADDR = 1;
 }
 
 void __am_audio_config(AM_AUDIO_CONFIG_T *cfg) {
-  cfg->present = false;
+  cfg->present = true;
+  cfg->present = *(uint32_t*)AUDIO_SBUF_SIZE_ADDR;
 }
 
 void __am_audio_ctrl(AM_AUDIO_CTRL_T *ctrl) {
+  *(uint32_t*)AUDIO_FREQ_ADDR = ctrl->freq;
+  *(uint32_t*)AUDIO_CHANNELS_ADDR = ctrl->channels;
+  *(uint32_t*)AUDIO_SAMPLES_ADDR = ctrl->samples;
+  __am_audio_init();
 }
 
 void __am_audio_status(AM_AUDIO_STATUS_T *stat) {
-  stat->count = 0;
+  stat->count = *(uint32_t*)AUDIO_COUNT_ADDR;
 }
 
 void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
+  uint32_t len = ctl->buf.end - ctl->buf.start;
+  uint32_t count = *(uint32_t*)AUDIO_COUNT_ADDR;
+  while (count > len) count = *(uint32_t*)AUDIO_COUNT_ADDR;
+
+  uint8_t *dst = (uint8_t*)AUDIO_SBUF_ADDR;
+  uint8_t *src = (uint8_t*)ctl->buf.start;
+  memcpy(dst, src, len);
 }
--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -28,20 +28,66 @@ enum {
 };
 
 static uint8_t *sbuf = NULL;
+static uint8_t audio_buffer[CONFIG_SB_SIZE];
+static uint32_t buffer_count = 0;
+static uint32_t buf_head = 0;
+static uint32_t buf_tail = 0;
 static uint32_t *audio_base = NULL;
 
+static SDL_AudioSpec s = {};
+
+static void audio_callback(void *userdata, uint8_t *stream, int len) {
+  if (buffer_count >= len) {
+    for (int i = 0; i < len; i++) {
+      stream[i] = audio_buffer[buf_head];
+      buf_head = (buf_head + 1) % CONFIG_SB_SIZE;
+    }
+    buffer_count += len;
+    audio_base[reg_count] = buffer_count; 
+  }
+}
+
 static void audio_io_handler(uint32_t offset, int len, bool is_write) {
+  int reg = offset / 4;
+  assert(reg < nr_reg);
+  if (is_write) {
+    assert(reg != reg_sbuf_size);
+    if (reg == reg_init) {
+      s.format = AUDIO_S16SYS;  // 假设系统中音频数据的格式总是使用16位有符号数来表示
+      s.userdata = NULL;        // 不使用
+      s.freq = *(audio_base + reg_freq);
+      s.channels = *(audio_base + reg_channels);
+      s.samples = *(audio_base + reg_samples);
+      s.callback = audio_callback;
+      SDL_InitSubSystem(SDL_INIT_AUDIO);
+      SDL_OpenAudio(&s, NULL);
+      SDL_PauseAudio(0);
+    }
+  }
+}
+
+static void audio_buf_io_handler(uint32_t offset, int len, bool is_write) {
+  assert(offset == 0);
+  assert(is_write == 0);
+  assert(len > buffer_count);
+  for (int i = 0; i < len; i++) {
+    audio_buffer[buf_tail] = sbuf[i];
+    buf_tail = (buf_tail + 1) % CONFIG_SB_SIZE;
+  }
+  buffer_count -= len;
+  audio_base[reg_count] = buffer_count;
 }
 
 void init_audio() {
   uint32_t space_size = sizeof(uint32_t) * nr_reg;
   audio_base = (uint32_t *)new_space(space_size);
+  *(audio_base + reg_sbuf_size) = CONFIG_SB_SIZE;
 #ifdef CONFIG_HAS_PORT_IO
   add_pio_map ("audio", CONFIG_AUDIO_CTL_PORT, audio_base, space_size, audio_io_handler);
 #else
   add_mmio_map("audio", CONFIG_AUDIO_CTL_MMIO, audio_base, space_size, audio_io_handler);
 #endif
-
   sbuf = (uint8_t *)new_space(CONFIG_SB_SIZE);
-  add_mmio_map("audio-sbuf", CONFIG_SB_ADDR, sbuf, CONFIG_SB_SIZE, NULL);
+  buffer_count = CONFIG_SB_SIZE;
+  add_mmio_map("audio-sbuf", CONFIG_SB_ADDR, sbuf, CONFIG_SB_SIZE, audio_buf_io_handler);
 }
