16aaa69386989c94e1afa5b743deebf0edd221ef
tracer-ics2023 <tracer@njuics.org>
2024-06-14 18:53:02 +0800

    >  compile NEMU 221220000 张三 Linux racheMint 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:53:02 up 55 min,  1 user,  load average: 1.35, 0.98, 0.54

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -11,12 +11,6 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg);
 
 void __am_gpu_init() {
   __am_gpu_config(&gpuConfig);
-  int i;
-  int w = gpuConfig.width; 
-  int h = gpuConfig.height;
-  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  for (i = 0; i < w * h; i ++) fb[i] = i;
-  outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
@@ -40,6 +34,12 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   // if (ctl->sync) {
   //   outl(SYNC_ADDR, 1);
   // }
+  int i;
+  int w = gpuConfig.width; 
+  int h = gpuConfig.height;
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  for (i = 0; i < w * h; i ++) fb[i] = i;
+  outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_status(AM_GPU_STATUS_T *status) {
