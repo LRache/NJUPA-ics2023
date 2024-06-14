13efc2fd17a624af7fc82d4bcfbaca39c93e0278
tracer-ics2023 <tracer@njuics.org>
2024-06-14 14:19:55 +0800

    >  compile NEMU 221220000 张三 Linux rache-Mint 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:19:55 up 22:42,  1 user,  load average: 0.86, 0.93, 0.79

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -10,7 +10,6 @@ typedef void (*fmt_fun_t)(char**, va_list*, char*);
 static void __fmt_s    (char **out, va_list *ap, char *arg);
 static void __fmt_d    (char **out, va_list *ap, char *arg);
 static void __fmt_llu  (char **out, va_list *ap, char *arg);
-static void __fmt_avoid(char **out, va_list *ap, char *arg);
 
 typedef struct {
   char fmt[10];
@@ -22,7 +21,6 @@ static FmtEntry fmtTable[] = {
   {"s"   , 1, __fmt_s},
   {"d"   , 1, __fmt_d},
   {"llu" , 3, __fmt_llu},
-  {"%"   , 1, __fmt_avoid}
 };
 
 #define FMT_TABLE_LEN (sizeof(fmtTable) / sizeof(fmtTable[0]))
@@ -37,16 +35,54 @@ static void __fmt_d(char **out, va_list *ap, char *arg) {
         
   if (d == 0) {
     *((*out)++) = '0';
-  } else {
-    int sign = d < 0;
-    if (sign) d = -d;
-    char stack[13] = {};
-    char *t = stack;
-    while (d) {
-      *(t++) = d % 10 + '0';
-      d = d / 10;
+    return;
+  }
+  int leftAlign = 0;
+  int minWidth = 0;
+  int zeroFill = 0;
+  if (*arg != 0) {
+    if (*arg == '0') {
+      zeroFill = 1;
+      arg++;
     }
-    char *h = stack;
+    if (*arg == '-') {
+      leftAlign = 1;
+      arg++;
+    } 
+    while (*arg) {
+      minWidth = minWidth * 10 + *arg - '0';
+      arg++;
+    }
+  } 
+
+  int sign = d < 0;
+  if (sign) d = -d;
+  
+  char stack[13] = {};
+  char *t = stack;
+  while (d) {
+    *(t++) = d % 10 + '0';
+    d = d / 10;
+  }
+
+  int width = t - stack;
+  char *h = stack;
+  if (width < minWidth) {
+    if (leftAlign) {
+      while (t > h) *((*out)++) = *(--t);
+      while (width <= minWidth) {
+        *((*out)++) = ' ';
+        width ++;
+      }
+    } else {
+      char fill = zeroFill ? '0' : ' ';
+      while (width <= minWidth) {
+        *((*out)++) = fill;
+        width++;
+      }
+      while (t > h) *((*out)++) = *(--t);
+    }
+  } else {
     if (sign) (*(*out)++) = '-';
     while (t > h) *((*out)++) = *(--t);
   }
@@ -67,10 +103,6 @@ static void __fmt_llu(char **out, va_list *ap, char *arg) {
     char *h = stack;
     while (t > h) *((*out)++) = *(--t);
   }
-}
-
-static void __fmt_avoid(char **out, va_list *ap, char *arg) {
-  *(*out)++ = '%';
 }
 
 int printf(const char *fmt, ...) {
@@ -94,9 +126,17 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
   {
     if (*p == '%') {
       p++;
+      if (*p == '%') {
+        *(out++) = '%';
+        continue;
+      }
+
+      char arg[10] = {};
+      for (int i = 0; isalpha(*p) && *p; i++, p++) arg[i] = *p;
+      
       for (int i = 0; i < FMT_TABLE_LEN; i++) {
         if (strncmp(p, fmtTable[i].fmt, fmtTable[i].length) == 0) {
-          fmtTable[i].fun(&out, &ap, NULL);
+          fmtTable[i].fun(&out, &ap, arg);
           p += fmtTable[i].length;
           break;
         } 
