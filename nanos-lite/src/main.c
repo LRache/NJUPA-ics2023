718916677f29ef72dd88c2bf6513c6e8f638d59f
tracer-ics2023 <tracer@njuics.org>
2024-06-20 21:50:33 +0800

    >  compile NEMU 221220000 张三 Linux rache-Mint 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:50:33 up  7:30,  1 user,  load average: 1.84, 1.52, 1.37

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -10,8 +10,8 @@ void init_proc(void);
 int main() {
   extern const char logo[];
   printf("%s", logo);
-  Log("'Hello World!' from Nanos-lite");
-  printf("A\n");
+  printf("'Hello World!' from Nanos-lite");
+  //Log("'Hello World!' from Nanos-lite");
   Log("Build time: %s, %s", __TIME__, __DATE__);
 
   init_mm();
