e000d1864b68fcc5ae39ccb8722e3d12c9ebea2e
tracer-ics2023 <tracer@njuics.org>
2024-06-20 21:12:36 +0800

    >  compile NEMU 221220000 张三 Linux rache-Mint 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:12:36 up  6:52,  1 user,  load average: 0.81, 1.18, 1.19

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -20,7 +20,6 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
    * Then return the address of the interrupt/exception vector.
    */
   cpu.mcause = NO;
-  Log("%x", cpu.mcause);
   cpu.mepc = epc;
   return cpu.mtvec;
 }
