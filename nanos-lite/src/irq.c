b8d76cbafeffd53f74383161cb3b6d262524b774
tracer-ics2023 <tracer@njuics.org>
2024-06-28 15:56:09 +0800

    >  compile NEMU 221220000 张三 Linux racheMint 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:56:09 up  5:09,  1 user,  load average: 0.97, 0.69, 0.63

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -3,12 +3,11 @@
 void do_syscall(Context *c);
 
 static Context* do_event(Event e, Context* c) {
-  // switch (e.event) {
-  //   case EVENT_YIELD: Log("Yield"); break;
-  //   case EVENT_SYSCALL: do_syscall(c); break;
-  //   default: panic("Unhandled event ID = %d", e.event);
-  // }
-  do_syscall(c);
+  switch (e.event) {
+    case EVENT_YIELD: Log("Yield"); break;
+    case EVENT_SYSCALL: do_syscall(c); break;
+    default: panic("Unhandled event ID = %d", e.event);
+  }
   return c;
 }
 
