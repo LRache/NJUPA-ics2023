bd23e9e169e3dc52b1fbeb494eda0f3b78f560fb
tracer-ics2023 <tracer@njuics.org>
2024-06-28 16:52:00 +0800

    >  compile NEMU 221220000 张三 Linux racheMint 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:52:00 up  6:04,  1 user,  load average: 0.66, 0.71, 0.50

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,5 +1,9 @@
 #include <common.h>
 #include "syscall.h"
+#include <inttypes.h>
+
+int sys_write(int fd, const void *buf, size_t count);
+
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
@@ -7,9 +11,25 @@ void do_syscall(Context *c) {
   a[2] = c->GPR3;
   a[3] = c->GPR4;
 
+  uintptr_t a0 = 0;
   switch (a[0]) {
-    case SYS_exit:  halt(a[1]); break;
-    case SYS_yield: yield();    break;
+    case SYS_exit:  halt(a[1]);       break;
+    case SYS_yield: yield();          break;
+    case SYS_write: 
+      a0 = sys_write(a[0], (void*)a[1], a[2]); 
+      break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
+  c->GPRx = a0;
+}
+
+int sys_write(int fd, const void *buf, size_t count) {
+  const char *buffer = (const char *)buf;
+  if (fd == 1 || fd == 2) {
+    for (int i = 0; i < count; i++) {
+      putch(buffer[i]);
+    }
+    return count;
+  }
+  return -1;
 }
