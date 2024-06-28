0a5922ddae42ac8494e5b295087e4cd06033a0b9
tracer-ics2023 <tracer@njuics.org>
2024-06-28 12:39:22 +0800

    >  compile NEMU 221220000 张三 Linux racheMint 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:39:22 up  1:52,  1 user,  load average: 0.32, 0.52, 0.52

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -9,8 +9,39 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
+#if defined(__ISA_AM_NATIVE__)
+# define EXPECT_ISA EM_X86_64
+#elif defined(__ISA_X86__)
+# define EXPECT_ISA EM_386
+#elif defined(__ISA_RISCV32__)
+# define EXPECT_ISA EM_RISCV
+#endif
+
+static const char ELF_MAGIC_NUMBER[] = {0x7f, 'E', 'L', 'F'};
+
+size_t ramdisk_read(void *buf, size_t offset, size_t len);
+size_t get_ramdisk_size();
+
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
+  size_t r;
+  Elf_Ehdr elfHeader;
+  r = ramdisk_read(&elfHeader, 0, sizeof(elfHeader));
+  assert(r == sizeof(elfHeader));
+  assert(memcmp(elfHeader.e_ident, ELF_MAGIC_NUMBER, 4) == 0);
+  assert(elfHeader.e_ident[EI_CLASS] == ELFCLASS32);
+  assert(elfHeader.e_machine == EXPECT_ISA);
+
+  Elf_Phdr phdrArray[elfHeader.e_phnum];
+  r = ramdisk_read(phdrArray, elfHeader.e_phoff, sizeof(phdrArray));
+  assert(r == sizeof(phdrArray));
+  
+  for (int i = 0; i < elfHeader.e_phnum; i++) {
+    Elf_Phdr phdr = phdrArray[i];
+    if (phdr.p_type == PT_LOAD) {
+      ramdisk_read((void*)(phdr.p_vaddr), phdr.p_offset, phdr.p_filesz);
+      memset((void*)(phdr.p_vaddr + phdr.p_filesz), 0, phdr.p_memsz - phdr.p_filesz); 
+    }
+  }
   return 0;
 }
 
