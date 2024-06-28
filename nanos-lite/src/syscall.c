1a45a66198b0c3e2f0f7fe67d87d622e2a8e9200
tracer-ics2023 <tracer@njuics.org>
2024-06-28 23:33:27 +0800

    >  compile NEMU 221220000 张三 Linux racheMint 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:33:27 up 12:46,  1 user,  load average: 1.70, 1.02, 0.76

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,8 +1,12 @@
 #include <common.h>
 #include "syscall.h"
-#include <inttypes.h>
+#include "fs.h"
 
+int sys_open(char *pathname, int flags, int mode);
+size_t sys_read(int fd, void *buf, size_t len);
 int sys_write(int fd, const void *buf, size_t count);
+int sys_close(int fd);
+size_t sys_lseek(int fd, off_t offset, int whence);
 int sys_brk();
 
 void do_syscall(Context *c) {
@@ -16,13 +20,29 @@ void do_syscall(Context *c) {
   switch (s) {
     case SYS_exit:  halt(arg[0]); break;
     case SYS_yield: yield();      break;
+    case SYS_open:
+      r = sys_open((char *)arg[0], arg[1], arg[2]); break;
+    case SYS_read:
+      r = sys_read(arg[0], (void *)arg[1], arg[2]); break;
     case SYS_write: 
-      r = sys_write(arg[0], (void*)arg[1], arg[2]); break;
+      r = sys_write(arg[0], (void *)arg[1], arg[2]); break;
+    case SYS_close:
+      r = sys_close(arg[0]); break;
+    case SYS_lseek:
+      r = sys_lseek(arg[0], arg[1], arg[2]); break;
     case SYS_brk:
       r = sys_brk(); break;
     default: panic("Unhandled syscall ID = %d", s);
   }
   c->GPRx = r;
+}
+
+int sys_open(char *pathname, int flags, int mode) {
+  return fs_open(pathname, flags, mode);
+}
+
+size_t sys_read(int fd, void *buf, size_t len) {
+  return fs_read(fd, buf, len);
 }
 
 int sys_write(int fd, const void *buf, size_t count) {
@@ -34,6 +54,14 @@ int sys_write(int fd, const void *buf, size_t count) {
     return count;
   }
   return -1;
+}
+
+int sys_close(int fd) {
+  return fs_close(fd);
+}
+
+size_t sys_lseek(int fd, off_t offset, int whence) {
+  return fs_lseek(fd, offset, whence);
 }
 
 int sys_brk() {
