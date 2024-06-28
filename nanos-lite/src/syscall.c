4ff5c6135c74c3a050b4dce9cb91781c6cc443c9
tracer-ics2023 <tracer@njuics.org>
2024-06-28 17:21:50 +0800

    >  compile NEMU 221220000 张三 Linux racheMint 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:21:50 up  6:34,  1 user,  load average: 0.70, 0.43, 0.30

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -25,7 +25,7 @@ void do_syscall(Context *c) {
 
 int sys_write(int fd, const void *buf, size_t count) {
   const char *buffer = (const char *)buf;
-  printf("%d\n", count);
+  printf("%u\n", count);
   if (fd == 1 || fd == 2) {
     for (int i = 0; i < count; i++) {
       putch(buffer[i]);
