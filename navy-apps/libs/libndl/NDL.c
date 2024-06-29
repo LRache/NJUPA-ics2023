462600cfe890f1870637e8592f26def591d560ef
tracer-ics2023 <tracer@njuics.org>
2024-06-29 18:21:33 +0800

    >  compile NEMU 221220000 张三 Linux racheMint 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:21:33 up  6:01,  1 user,  load average: 2.37, 1.70, 1.34

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -65,9 +65,10 @@ int NDL_QueryAudio() {
 }
 
 int NDL_Init(uint32_t flags) {
-  if (getenv("NWM_APP")) {
-    evtdev = 3;
-  }
+  // if (getenv("NWM_APP")) {
+  //   evtdev = 3;
+  // }
+  evtdev = 3;
   return 0;
 }
 
