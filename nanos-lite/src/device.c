de0040d8498916fdb7c536bd37b5cfc05a9fb3b4
tracer-ics2023 <tracer@njuics.org>
2024-06-29 15:52:55 +0800

    >  compile NEMU 221220000 张三 Linux racheMint 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:52:55 up  3:32,  1 user,  load average: 3.41, 2.09, 1.83

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -24,8 +24,8 @@ size_t serial_write(const void *buf, size_t offset, size_t len) {
 
 size_t events_read(void *buf, size_t offset, size_t len) {
   AM_INPUT_KEYBRD_T e = io_read(AM_INPUT_KEYBRD);
-  Log("%d", e.keycode);
   if (e.keycode == 0) return 0;
+  Log("%d", e.keycode);
 
   char buffer[3] = {[0]= 'k', [2] = ' '};
   if (e.keydown) buffer[1] = 'd';
