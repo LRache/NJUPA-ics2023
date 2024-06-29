f2896c352a4f1da5ad63084ad304a97f200b178f
tracer-ics2023 <tracer@njuics.org>
2024-06-29 21:26:30 +0800

    >  compile NEMU 221220000 张三 Linux racheMint 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:26:30 up 5 min,  1 user,  load average: 1.11, 0.70, 0.32

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -72,6 +72,9 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
   Log("%u %d %d %d", offset, x, y, count);
   int i = 0;
   for (; i < count; i++) {
+    if (((uint32_t*)buf)[i] != 0x00ffffff) {
+      Log("A");
+    }
     io_write(AM_GPU_FBDRAW, canvas_x + x, canvas_y + y, &((uint32_t*)buf)[i], 1, 1, false);
     x ++;
     y += (x == canvas_width);
