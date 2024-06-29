1541abd9f9ac0e753dfd7e9e8c1fb7aefd6c411c
tracer-ics2023 <tracer@njuics.org>
2024-06-29 18:47:56 +0800

    >  compile NEMU 221220000 张三 Linux racheMint 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:47:56 up  6:27,  1 user,  load average: 0.24, 0.32, 0.61

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -45,6 +45,7 @@ void NDL_OpenCanvas(int *w, int *h) {
 void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
   for (int x_ = 0; x_ < h; x_++) {
     int offset = ((x + x_) * screen_w + y) * 4;
+    lseek(5, offset, SEEK_SET);
     write(5, pixels, w);
   }
 }
