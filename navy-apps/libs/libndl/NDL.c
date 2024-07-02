4dbdd2cda0d0dc249a2b01c2657c0308cd6d2159
tracer-ics2023 <tracer@njuics.org>
2024-07-02 10:47:21 +0800

    >  compile NEMU 221220000 张三 Linux rache-Mint 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:47:21 up 12:44,  1 user,  load average: 0.47, 0.49, 0.39

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -20,23 +20,27 @@ int NDL_PollEvent(char *buf, int len) {
 }
 
 void NDL_OpenCanvas(int *w, int *h) {
-  //if (getenv("NWM_APP")) {
-    int fbctl = 4;
-    fbdev = 5;
+  fbdev = 5;
+  int fbctl = 4;
+  
+  char buf[64];
+  if (*w == 0 && *h == 0) {
+    read(6, buf, sizeof(buf));
+    sscanf(buf, "%d %d", &screen_w, &screen_h);
+  } else {
     screen_w = *w; screen_h = *h;
-    char buf[64];
-    int len = sprintf(buf, "%d %d", screen_w, screen_h);
-    // let NWM resize the window and create the frame buffer
-    write(fbctl, buf, len);
-    while (1) {
-      // 3 = evtdev
-      int nread = read(3, buf, sizeof(buf) - 1);
-      if (nread <= 0) continue;
-      buf[nread] = '\0';
-      if (strcmp(buf, "mmap ok") == 0) break;
-    }
-    close(fbctl);
-  //}
+  }
+  int len = sprintf(buf, "%d %d", screen_w, screen_h);
+  // let NWM resize the window and create the frame buffer
+  write(fbctl, buf, len);
+  while (1) {
+    // 3 = evtdev
+    int nread = read(3, buf, sizeof(buf) - 1);
+    if (nread <= 0) continue;
+    buf[nread] = '\0';
+    if (strcmp(buf, "mmap ok") == 0) break;
+  }
+  close(fbctl);
 }
 
 void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
