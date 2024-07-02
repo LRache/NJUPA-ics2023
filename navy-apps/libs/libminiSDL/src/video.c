b80c8738fe5c5f4594d0c548ff1298b738753bbd
tracer-ics2023 <tracer@njuics.org>
2024-07-02 10:14:54 +0800

    >  compile NEMU 221220000 张三 Linux rache-Mint 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:14:54 up 12:12,  1 user,  load average: 0.44, 0.31, 0.32

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -3,6 +3,7 @@
 #include <assert.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_Rect *dstrect) {
   assert(dst && src);
@@ -22,9 +23,10 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
     if (dstrect == NULL) {dx = 0; dy = 0; }
     else {dx = dstrect->x; dy = dstrect->y; }
 
+    printf("%p %p\n", dst->pixels, src->pixels);
     for (int h = 0; h < height; h++) {
       for (int w = 0; w < width; w++) {
-        // *((uint32_t *)dst->pixels + (dy + h) * dst->w + (dx + w)) = *((uint32_t *)src->pixels + (sy + h) * src->w + (sx + w));
+        *((uint32_t *)dst->pixels + (dy + h) * dst->w + (dx + w)) = *((uint32_t *)src->pixels + (sy + h) * src->w + (sx + w));
       }
     }
   }
