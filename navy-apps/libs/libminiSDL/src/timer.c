cb00c406d0b15377c10cbc21b1313ab31f982b47
tracer-ics2023 <tracer@njuics.org>
2024-07-02 12:04:54 +0800

    >  compile NEMU 221220000 张三 Linux rache-Mint 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:04:54 up 14:02,  1 user,  load average: 0.26, 0.54, 0.50

--- navy-apps/libs/libminiSDL/src/timer.c
+++ navy-apps/libs/libminiSDL/src/timer.c
@@ -12,7 +12,7 @@ int SDL_RemoveTimer(SDL_TimerID id) {
 
 extern uint32_t SDL_startTicks;
 uint32_t SDL_GetTicks() {
-  return SDL_startTicks - NDL_GetTicks();
+  return NDL_GetTicks() - SDL_startTicks;
 }
 
 void SDL_Delay(uint32_t ms) {
