37c1cd5db27975e440891cb6c9c71ca6e0688034
tracer-ics2023 <tracer@njuics.org>
2024-07-02 10:55:06 +0800

    >  compile NEMU 221220000 张三 Linux rache-Mint 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:55:06 up 12:52,  1 user,  load average: 0.58, 0.50, 0.45

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -14,14 +14,32 @@ int SDL_PushEvent(SDL_Event *ev) {
   return 0;
 }
 
-int SDL_PollEvent(SDL_Event *ev) {
-  return 0;
+int SDL_PollEvent(SDL_Event *event) {
+  char buffer[16];
+  int r = read(3, buffer, sizeof(buffer));
+  if (r == 0) return 0;
+
+  char type[6], arg[12];
+  sscanf(buffer, "%s %s", type, arg);
+  if (strcmp(type, "mmap") == 0) {
+    event->type = SDL_USEREVENT;
+  } 
+  else if (strcmp(type, "kd") == 0 || strcmp(type, "ku") == 0) {
+    if (type[1] == 'u') event->type = SDL_KEYUP;
+    else if (type[1] == 'd') event->type = SDL_KEYDOWN;
+    for (int i = 0; i < SDLK_COUNT; i++) {
+      if (strcmp(arg, keyname[i]) == 0) {
+        event->key.keysym.sym = i;
+        break;
+      }
+    }
+  }
+  return 1;
 }
 
 int SDL_WaitEvent(SDL_Event *event) {
   char buffer[16];
-  int r = read(3, buffer, sizeof(buffer));
-  if (r == 0) return 0;
+  while (read(3, buffer, sizeof(buffer)) == 0);
 
   char type[6], arg[12];
   sscanf(buffer, "%s %s", type, arg);
