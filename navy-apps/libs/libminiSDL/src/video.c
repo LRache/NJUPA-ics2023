c23a925a942c0d90ac2836b31853790581bd436c
tracer-ics2023 <tracer@njuics.org>
2024-07-02 10:50:43 +0800

    >  compile NEMU 221220000 张三 Linux rache-Mint 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:50:43 up 12:47,  1 user,  load average: 0.40, 0.54, 0.44

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -23,11 +23,11 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
     if (dstrect == NULL) {dx = 0; dy = 0; }
     else {dx = dstrect->x; dy = dstrect->y; }
 
-    printf("%d %d %d %d\n", sx, sy, dx, dy);
+    //printf("%d %d %d %d\n", sx, sy, dx, dy);
     for (int h = 0; h < height; h++) {
       for (int w = 0; w < width; w++) {
         //printf("%d %d\n", (dy + h) * dst->w + (dx + w), (sy + h) * src->w + (sx + w));
-        //*((uint32_t *)dst->pixels + (dy + h) * dst->w + (dx + w)) = *((uint32_t *)src->pixels + (sy + h) * src->w + (sx + w));
+        *((uint32_t *)dst->pixels + (dy + h) * dst->w + (dx + w)) = *((uint32_t *)src->pixels + (sy + h) * src->w + (sx + w));
       }
     }
   }
