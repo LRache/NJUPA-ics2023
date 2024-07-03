68f381d0b9b6961a8da76bab7eebb7b518495660
tracer-ics2023 <tracer@njuics.org>
2024-07-03 22:08:30 +0800

    >  compile NEMU 221220000 张三 Linux rache-Mint 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:08:30 up  8:09,  1 user,  load average: 1.47, 1.38, 1.43

--- nemu/src/device/local.c
+++ nemu/src/device/local.c
@@ -110,7 +110,7 @@ static void local_write() {
         return ;
     }
     FILE *f = files[fd];
-    printf("%d\n", f == stdout);
+    printf("%d\n", fd);
     size_t length = ctl[reg_arg] < LOCAL_BUF_SIZE ? ctl[reg_arg] : LOCAL_BUF_SIZE;
     ctl[reg_arg] = fwrite(buf, 1, length, f);
 }
