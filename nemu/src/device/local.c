967ce6811dc9bf59024512dcd23685182c113d83
tracer-ics2023 <tracer@njuics.org>
2024-07-03 21:54:21 +0800

    >  compile NEMU 221220000 张三 Linux rache-Mint 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:54:21 up  7:55,  1 user,  load average: 1.10, 1.54, 1.43

--- nemu/src/device/local.c
+++ nemu/src/device/local.c
@@ -101,7 +101,7 @@ static void local_read() {
     FILE *f = files[fd];
     size_t length = ctl[reg_arg] < LOCAL_BUF_SIZE ? ctl[reg_arg] : LOCAL_BUF_SIZE;
     ctl[reg_arg] = fread(buf, 1, length, f);
-    printf("Read %d bytes %p\n", ctl[reg_arg], f);
+    printf("Read %ld bytes\n", length);
 }
 
 static void local_ctl_handler(uint32_t offset, int len, bool is_write) {
