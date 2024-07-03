0bbfe7f8ae18153623a5e31846f01719be121431
tracer-ics2023 <tracer@njuics.org>
2024-07-03 13:50:13 +0800

    >  compile NEMU 221220000 张三 Linux rache-Mint 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:50:13 up 26 min,  1 user,  load average: 0.42, 0.31, 0.21

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -129,7 +129,7 @@ static int decode_exec(Decode *s) {
   INSTPAT_END();
 
   R(0) = 0; // reset $zero to 0
-  if (s->pc == 0x830067d4)
+  if (s->pc == 0x83006860)
   Log("%x", s->dnpc);
 
   return 0;
