f58c4bea546561ea459a12ac7d462f692863b803
tracer-ics2023 <tracer@njuics.org>
2024-07-04 23:40:44 +0800

    >  compile NEMU 221220000 张三 Linux rache-Mint 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:40:44 up  3:38,  1 user,  load average: 1.33, 1.44, 1.37

--- navy-apps/tests/local-test/main.c
+++ navy-apps/tests/local-test/main.c
@@ -4,9 +4,9 @@
 
 int main() {
     const char *filepath = "/home/rache/Documents/ics2023/hello.txt";
-    int fd = open(filepath, O_RDONLY);
+    FILE *f = fopen(filepath, "r");
     char buffer[64];
-    size_t nbytes = read(fd, buffer, 63);
+    size_t nbytes = fread(buffer, 1, 63, f);
     buffer[nbytes] = 0;
     printf("%s\n", buffer);
     return 0;
