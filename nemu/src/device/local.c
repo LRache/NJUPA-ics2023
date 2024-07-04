f9811fc4eb2f7cc157cc1e463bf8df8fada9bd90
tracer-ics2023 <tracer@njuics.org>
2024-07-04 23:38:26 +0800

    >  compile NEMU 221220000 张三 Linux rache-Mint 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:38:26 up  3:36,  1 user,  load average: 1.44, 1.55, 1.39

--- nemu/src/device/local.c
+++ nemu/src/device/local.c
@@ -72,8 +72,7 @@ static void local_open() {
         return;
     }
 
-    Log("%s", buf);
-    int local_fd = open(buf, ctl[reg_arg]);
+    int local_fd = open(arg, ctl[reg_arg]);
     if (local_fd == -1) {
         ctl[reg_fd] = -1;
     } else {
