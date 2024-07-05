1dbfd6b65a30c4770fe75d185900a0c16ff1a1d1
tracer-ics2023 <tracer@njuics.org>
2024-07-05 19:23:16 +0800

    >  compile NEMU 221220000 张三 Linux rache-Mint 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:23:16 up  2:44,  1 user,  load average: 0.85, 0.40, 0.29

--- navy-apps/tests/cpp-test/main.cpp
+++ navy-apps/tests/cpp-test/main.cpp
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+#define HAVE_INITFINI_ARRAY
+
 class Test {
 public:
   Test()  { printf("%s,%d: Hello, Project-N!\n", __func__, __LINE__); }
@@ -9,9 +11,6 @@ public:
 Test test;
 
 int main() {
-#ifdef HAVE_INITFINI_ARRAY
-  printf("HAS\n");
-#endif
   printf("%s,%d: Hello world!\n", __func__, __LINE__);
   return 0;
 }
