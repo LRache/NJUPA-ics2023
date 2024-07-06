48a5a289dd39eeab0cf1165404c0f63b7deed1f9
tracer-ics2023 <tracer@njuics.org>
2024-07-06 22:00:00 +0800

    >  compile NEMU 221220000 张三 Linux rache-Mint 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:00:00 up 14:07,  1 user,  load average: 1.47, 1.56, 0.96

--- navy-apps/libs/libminiSDL/src/audio.c
+++ navy-apps/libs/libminiSDL/src/audio.c
@@ -64,7 +64,7 @@ SDL_AudioSpec *SDL_LoadWAV(const char *file, SDL_AudioSpec *spec, uint8_t **audi
   uint32_t chunckID;
   Read(chunckID, 4);
   printf("0x%x\n", chunckID);
-  if (chunckID != 0x46465952) {
+  if (chunckID != 0x46464952) {
     return NULL;
   }
   printf("continue\n");
