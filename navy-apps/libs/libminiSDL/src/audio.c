ea0979f99a3a485d0115f8e3aad8630576e38902
tracer-ics2023 <tracer@njuics.org>
2024-07-06 20:36:20 +0800

    >  compile NEMU 221220000 张三 Linux rache-Mint 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:36:20 up 12:43,  1 user,  load average: 1.97, 1.81, 1.33

--- navy-apps/libs/libminiSDL/src/audio.c
+++ navy-apps/libs/libminiSDL/src/audio.c
@@ -1,6 +1,8 @@
 #include <NDL.h>
 #include <SDL.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
 
 #define BUF_SIZE 4096
 
@@ -39,13 +41,58 @@ void SDL_PauseAudio(int pause_on) {
 }
 
 void SDL_MixAudio(uint8_t *dst, uint8_t *src, uint32_t len, int volume) {
+  int16_t *_dst = (int16_t *)dst;
+  int16_t *_src = (int16_t *)src;
+  int32_t _len = len / 2;
+  for (int i = 0; i < _len; i++) {
+    int32_t v = (int32_t) *_src + *_dst * volume / SDL_MIX_MAXVOLUME;
+    if (v < -32768) v = -32768;
+    else if (v > 32767) v = 32767;
+    *_src = (int16_t)v;
+    _src++;
+    _dst++;
+  }
 }
 
+#define Read(b, s) r = read(fd, &b, s); if (r != s) return NULL;
+#define Seek(s) r = lseek(fd, s, SEEK_SET); if (r != s) return NULL;
+
 SDL_AudioSpec *SDL_LoadWAV(const char *file, SDL_AudioSpec *spec, uint8_t **audio_buf, uint32_t *audio_len) {
-  return NULL;
+  int fd = open(file, O_RDONLY);
+  int r;
+  uint32_t chunckID;
+  Read(chunckID, 4);
+  if (chunckID != 0x46465952) {
+    return NULL;
+  }
+  uint16_t channels, freq, samples;
+  Seek(22);
+  Read(channels, 2);
+  Read(freq, 2);
+  Seek(34);
+  Read(samples, 2);
+  
+  uint32_t dataLength;
+  Seek(40);
+  Read(dataLength, 4);
+  
+  uint8_t *buffer = malloc(dataLength);
+  r = read(fd, buffer, dataLength);
+  if (r != dataLength) {
+    free(buffer);
+    return NULL;
+  }
+
+  *audio_buf = buffer;
+  *audio_len = dataLength;
+  spec->freq = freq;
+  spec->samples = samples;
+  spec->channels = channels;
+  return spec;
 }
 
 void SDL_FreeWAV(uint8_t *audio_buf) {
+  free(audio_buf);
 }
 
 void SDL_LockAudio() {
