7e63e06f5817869ef489a55bdc545ee855e0ca54
tracer-ics2023 <tracer@njuics.org>
2024-07-07 20:23:31 +0800

    >  compile NEMU 221220000 张三 Linux rache-Mint 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:23:31 up  9:32,  2 users,  load average: 0.78, 0.62, 0.58

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -35,9 +35,6 @@ void init_proc() {
   context_kload(&pcb[0], hello_fun, (void *)1);
   context_kload(&pcb[1], hello_fun, (void *)2);
 
-  for (int i = 0; i < 2; i++) {
-    Log("%d %p", i, pcb[i].cp);
-  }
   yield();
 }
 
