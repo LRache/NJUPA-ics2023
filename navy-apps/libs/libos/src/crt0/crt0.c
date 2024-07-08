a4b4b2f82b21cff06a67fd56fa550b9b0e421610
tracer-ics2023 <tracer@njuics.org>
2024-07-08 12:23:39 +0800

    >  compile NEMU 221220000 张三 Linux rache-Mint 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:23:39 up  2:44,  1 user,  load average: 0.84, 0.47, 0.42

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -4,12 +4,12 @@
 
 int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
-void call_main(uintptr_t *sp) {
+void call_main(uintptr_t *args) {
   char *empty[] =  {NULL };
   environ = empty;
-  int argc = *(int *)sp;
-  char **args = (char **)(sp+1);
-  char **envp = (char **)(sp + argc + 1);
+  int argc = *(int *)args;
+  char **args = (char **)(args + 1);
+  char **envp = (char **)(args + argc + 2);
   int r = main(argc, args, envp);
   exit(r);
   assert(0);
