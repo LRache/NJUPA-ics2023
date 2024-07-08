eab260a05bda01d437ae47d253bda8285a25ecbe
tracer-ics2023 <tracer@njuics.org>
2024-07-08 12:14:23 +0800

    >  compile NEMU 221220000 张三 Linux rache-Mint 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:14:23 up  2:35,  1 user,  load average: 0.49, 0.65, 0.50

--- navy-apps/tests/mainargs-test/main.c
+++ navy-apps/tests/mainargs-test/main.c
@@ -3,12 +3,12 @@
 int main(int argc, char **argv, char **envp) {
     printf("argc=%d\n", argc);
     for (int i = 0; i < argc; i++) {
-        printf("%s", argv[i]);
+        printf("%s\n", argv[i]);
     }
     printf("ENVP:\n");
     char **e = envp;
     while (e) {
-        printf("%s", *e);
+        printf("%s\n", *e);
         e++;
     }
     return 0;
