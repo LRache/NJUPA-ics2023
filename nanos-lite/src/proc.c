059bbdc09c2904ac108ce42d0319e01abee2ad71
tracer-ics2023 <tracer@njuics.org>
2024-07-08 19:10:25 +0800

    >  compile NEMU 221220000 张三 Linux rache-Mint 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:10:25 up 29 min,  1 user,  load average: 0.41, 0.17, 0.17

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -61,7 +61,7 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
 
 void execve(const char *filename, char *const argv[], char *const envp[]) {
   context_uload(current, filename, argv, envp);
-  //switch_boot_pcb();
+  switch_boot_pcb();
   yield();
 }
 
