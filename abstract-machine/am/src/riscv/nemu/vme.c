389936d240b4b006af15fb2f83899ba4a829097e
tracer-ics2023 <tracer@njuics.org>
2024-07-12 14:30:10 +0800

    >  compile NEMU 221220000 张三 Linux rache-Mint 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:30:10 up  5:40,  2 users,  load average: 1.41, 0.89, 0.83

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -87,7 +87,7 @@ void map(AddrSpace *as, void *va, void *pa, int prot) {
     ppn = pt[vpn[1]] >> 10;
   }
   pt = (uint32_t *)(ppn * PGSIZE);
-  printf("%p\n", pt);
+  printf("%p\n", ppn);
   pt[vpn[0]] = ((paddr & (as->pgsize - 1)) >> 2) | 0xf;
 }
 
