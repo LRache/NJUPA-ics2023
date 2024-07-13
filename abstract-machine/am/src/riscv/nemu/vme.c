d88e3dd6e37f35a2a2d2b1fc71b2843285f2400a
tracer-ics2023 <tracer@njuics.org>
2024-07-13 21:56:11 +0800

    >  compile NEMU 221220000 张三 Linux rache-Mint 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:56:11 up  6:04,  1 user,  load average: 1.12, 0.82, 0.97

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -93,7 +93,7 @@ void map(AddrSpace *as, void *va, void *pa, int prot) {
 Context *ucontext(AddrSpace *as, Area kstack, void *entry) {
   Context *context = (Context *)(kstack.end - sizeof(Context));
   context->mepc = (uintptr_t)entry - 4;
-  context->mstatus = 0x1800 | (1 << 3);
+  context->mstatus = 0x1800 | (1 << 3) | (1 << 7);
   context->pdir = as->ptr;
   printf("Ucontext: 0x%x\n", context->mstatus);
   return context;
