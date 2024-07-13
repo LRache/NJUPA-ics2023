661d9eafcef4338a96b14c46e73fcf559ec2f6aa
tracer-ics2023 <tracer@njuics.org>
2024-07-13 17:49:38 +0800

    >  compile NEMU 221220000 张三 Linux rache-Mint 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:49:38 up  1:58,  1 user,  load average: 2.18, 1.55, 1.04

--- abstract-machine/am/src/platform/nemu/ioe/local.c
+++ abstract-machine/am/src/platform/nemu/ioe/local.c
@@ -66,6 +66,7 @@ void __am_local_seek(AM_LOCAL_SEEK_T *c) {
     *(int64_t *)LOCAL_BUF_ADDR = c->offset;
     __execute();
     volatile uint64_t ret = *(uint64_t *)LOCAL_BUF_ADDR;
-    printf("a=%llu\n", *(uint64_t *)LOCAL_BUF_ADDR);
+    printf("a=%u\n", *(uint32_t *)LOCAL_BUF_ADDR);
+    printf("a=%u\n", *(((uint32_t *)LOCAL_BUF_ADDR) + 1));
     *c->ret = ret;
 }
