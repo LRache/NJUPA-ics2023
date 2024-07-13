7dba93c7198e042e62dbe3382ce2254758e71e04
tracer-ics2023 <tracer@njuics.org>
2024-07-13 22:15:09 +0800

    >  compile NEMU 221220000 张三 Linux rache-Mint 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:15:09 up  6:23,  1 user,  load average: 1.86, 1.80, 1.65

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -5,9 +5,10 @@ void do_syscall(Context *c);
 
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
-    case EVENT_YIELD:   c = schedule(c); break;
-    case EVENT_IRQ_TIMER: Log("IRQ_TIMER"); break;
-    case EVENT_SYSCALL: do_syscall(c); break;
+    case EVENT_YIELD: case EVENT_IRQ_TIMER: 
+      c = schedule(c); break;
+    case EVENT_SYSCALL: 
+      do_syscall(c); break;
     default: panic("Unhandled event ID = %d", e.event);
   }
   return c;
