ec6d5ffb29a30aaf884a93966fc294daf7f25f74
tracer-ics2023 <tracer@njuics.org>
2024-07-14 10:40:30 +0800

    >  compile NEMU 221220000 张三 Linux rache-Mint 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:40:30 up  1:28,  1 user,  load average: 0.25, 0.38, 0.48

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -98,7 +98,7 @@ void init_proc() {
   //naive_uload(NULL, "/bin/cpp-test");
   // char *const argv[] = {"/bin/hplayer", "/home/rache/Music/xitiejie.pcm", NULL};
   char *const empty[] = {NULL};
-  context_uload(&pcb[0], "/bin/hello", empty, empty);
+  context_kload(&pcb[0], hello_fun, (void *)1);
   context_uload(&pcb[1], "/bin/hello", empty, empty);
   running[0] = &pcb[0];
   running[1] = &pcb[1];
