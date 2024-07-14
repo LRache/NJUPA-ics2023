545da35a6197d90850bb4e30c6b16750533ae5ca
tracer-ics2023 <tracer@njuics.org>
2024-07-14 10:38:03 +0800

    >  compile NEMU 221220000 张三 Linux rache-Mint 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:38:03 up  1:25,  1 user,  load average: 0.41, 0.47, 0.53

---
+++ env.sh
@@ -0,0 +1,3 @@
+export NEMU_HOME=/home/rache/Documents/code/ics2023/nemu
+export NAVY_HOME=/home/rache/Documents/code/ics2023/navy-apps
+export AM_HOME=/home/rache/Documents/code/ics2023/abstract-machine
--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -96,10 +96,10 @@ void init_proc() {
 
   // load program here
   //naive_uload(NULL, "/bin/cpp-test");
-  char *const argv[] = {"/bin/hplayer", "/home/rache/Music/xitiejie.pcm", NULL};
+  // char *const argv[] = {"/bin/hplayer", "/home/rache/Music/xitiejie.pcm", NULL};
   char *const empty[] = {NULL};
   context_uload(&pcb[0], "/bin/hello", empty, empty);
-  context_uload(&pcb[1], "/bin/hplayer", argv, empty);
+  context_uload(&pcb[1], "/bin/hello", empty, empty);
   running[0] = &pcb[0];
   running[1] = &pcb[1];
   runningCount = 2;
